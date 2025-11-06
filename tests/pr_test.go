7dbefb5f3e816d5fd7bcd80128cf87a03be26369
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-11-06 12:00:31 +0530

    test: updated test to include assertions

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -3,6 +3,7 @@ package test
 
 import (
 	"math/rand"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,6 +13,7 @@ import (
 const crnParserExample = "examples/crn-parser"
 const getImagesExample = "examples/image-selector"
 
+var validArchitectures = []string{"amd64", "s390x"}
 var validRegions = []string{
 	"us-south",
 	"br-sao",
@@ -52,10 +54,20 @@ func TestRunSelectLatestImageExample(t *testing.T) {
 
 	options := setupOptions(t, getImagesExample)
 	options.TerraformVars = map[string]interface{}{
-		"region": validRegions[rand.Intn(len(validRegions))],
+		"region":       validRegions[rand.Intn(len(validRegions))],
+		"architecture": validArchitectures[rand.Intn(len(validArchitectures))],
 	}
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
+	if output != nil {
+		// imageName := output["latest_image_name"]
+		imageName := output.RawPlan.OutputChanges["latest_image_name"].After.(string)
+		assert.Contains(t, imageName, "ubuntu", "Image name should include 'ubuntu'")
+
+		// Check that image name contains either "amd64" or "s390x"
+		isValidArch := strings.Contains(imageName, "amd64") || strings.Contains(imageName, "s390x")
+		assert.True(t, isValidArch, "Image name should include either 'amd64' or 's390x'")
+	}
 }
